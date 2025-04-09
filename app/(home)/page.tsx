e58eb85688060ec79c9e5a36dc06599f66c01002
André Luiz <adventureandre@hotmail.com>
2025-04-09 08:14:16 -0300

    adiciona botão de vídeo com novo estilo e reorganiza a estrutura do carrossel

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { LuCheck, LuSnowflake, LuStar, LuArrowLeft, LuArrowRight, LuX } from "react-icons/lu";
+import { LuCheck, LuSnowflake,LuPlay, LuStar, LuArrowLeft, LuArrowRight, LuX } from "react-icons/lu";
 import { useInView } from "react-intersection-observer";
 import Snowfall from "react-snowfall";
 import { Parallax } from "react-parallax";
@@ -88,10 +88,12 @@ export default function Home() {
             <div className="py-10">
               <button
                 onClick={() => setIsModalOpen(true)}
-                className="text-white underline hover:text-gray-300"
+                className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-full shadow-lg animate-bounce flex items-center gap-2 transition-all duration-300"
               >
+                <LuPlay size={24} />
                 Clique aqui e assista ao vídeo
               </button>
+
             </div>
 
             {isModalOpen && (
@@ -162,7 +164,7 @@ export default function Home() {
 
           <section className=" bg-black/50">
             <div className="relative container mx-auto max-w-7xl px-4 py-10">
-            <h1 className="text-4xl my-5 text-blue-400">O maior parque de neve indoor da América Latina: a principal atração do Ski Planet</h1>
+              <h1 className="text-4xl my-5 text-blue-400">O maior parque de neve indoor da América Latina: a principal atração do Ski Planet</h1>
 
               <Swiper
                 slidesPerView={1}
@@ -179,50 +181,50 @@ export default function Home() {
               >
                 {/* SwiperSlides */}
                 <SwiperSlide >
-                <article className="flex flex-col justify-center items-center gap-2">
-                  <Image
-                    src="/esportes-na-neve_compress.jpg"
-                    alt="Imagem do parque"
-                    width={400}
-                    height={300}
-                    className="rounded-lg shadow-lg"
-                  />
-                  <h2 className="text-2xl font-bold text-white">Esportes na Neve</h2>
-                  <p className="text-white">
-                    Pista ampla para esqui e snowboard
-                  </p>
-                </article>
-              </SwiperSlide>
-              <SwiperSlide >
-                <article className="flex flex-col justify-center items-center gap-2">
-                  <Image
-                    src="/lazer-e-entretenimento_compress.jpg"
-                    alt="Imagem do parque"
-                    width={400}
-                    height={300}
-                    className="rounded-lg shadow-lg"
-                  />
-                  <h2 className="text-2xl font-bold text-white">Lazer e Entretenimento</h2>
-                  <p className="text-white">
-                    Atividades interativas e atrações temáticas para todas as idades
-                  </p>
-                </article>
-              </SwiperSlide>
-              <SwiperSlide >
-                <article className="flex flex-col justify-center items-center gap-2">
-                  <Image
-                    src="/gastronomia-tematica_compress.jpg"
-                    alt="Imagem do parque"
-                    width={400}
-                    height={300}
-                    className="rounded-lg shadow-lg"
-                  />
-                  <h2 className="text-2xl font-bold text-white">Gastronomia Temática</h2>
-                  <p className="text-white">
-                    Experiências culinárias imersivas
-                  </p>
-                </article>
-              </SwiperSlide>
+                  <article className="flex flex-col justify-center items-center gap-2">
+                    <Image
+                      src="/esportes-na-neve_compress.jpg"
+                      alt="Imagem do parque"
+                      width={400}
+                      height={300}
+                      className="rounded-lg shadow-lg"
+                    />
+                    <h2 className="text-2xl font-bold text-white">Esportes na Neve</h2>
+                    <p className="text-white">
+                      Pista ampla para esqui e snowboard
+                    </p>
+                  </article>
+                </SwiperSlide>
+                <SwiperSlide >
+                  <article className="flex flex-col justify-center items-center gap-2">
+                    <Image
+                      src="/lazer-e-entretenimento_compress.jpg"
+                      alt="Imagem do parque"
+                      width={400}
+                      height={300}
+                      className="rounded-lg shadow-lg"
+                    />
+                    <h2 className="text-2xl font-bold text-white">Lazer e Entretenimento</h2>
+                    <p className="text-white">
+                      Atividades interativas e atrações temáticas para todas as idades
+                    </p>
+                  </article>
+                </SwiperSlide>
+                <SwiperSlide >
+                  <article className="flex flex-col justify-center items-center gap-2">
+                    <Image
+                      src="/gastronomia-tematica_compress.jpg"
+                      alt="Imagem do parque"
+                      width={400}
+                      height={300}
+                      className="rounded-lg shadow-lg"
+                    />
+                    <h2 className="text-2xl font-bold text-white">Gastronomia Temática</h2>
+                    <p className="text-white">
+                      Experiências culinárias imersivas
+                    </p>
+                  </article>
+                </SwiperSlide>
               </Swiper>
 
               <button className="hidden md:flex ml-[-60px] custom-prev absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-800 dark:bg-gray-300 text-white dark:text-black p-3 rounded-full shadow-lg z-10">
@@ -236,25 +238,25 @@ export default function Home() {
         </Parallax>
 
         <section className=" bg-[#1d5491]">
-            <div className="relative container mx-auto max-w-7xl px-4 py-10">
+          <div className="relative container mx-auto max-w-7xl px-4 py-10">
             <h1 className="text-4xl my-5 text-blue-400 w-full text-center">E tem muito mais!
-            Diversão e entretenimento em nível internacional</h1>
+              Diversão e entretenimento em nível internacional</h1>
 
-              <Swiper
-                slidesPerView={1}
-                breakpoints={{
-                  768: { slidesPerView: 2 },
-                  1024: { slidesPerView: 3 },
-                }}
-                spaceBetween={15}
-                modules={[Navigation, Pagination, Autoplay]}
-                navigation={{ nextEl: ".custom-next", prevEl: ".custom-prev" }}
-                pagination={{ clickable: true }}
-                autoplay={{ delay: 3000 }}
-                loop
-              >
-                {/* SwiperSlides */}
-                <SwiperSlide >
+            <Swiper
+              slidesPerView={1}
+              breakpoints={{
+                768: { slidesPerView: 2 },
+                1024: { slidesPerView: 3 },
+              }}
+              spaceBetween={15}
+              modules={[Navigation, Pagination, Autoplay]}
+              navigation={{ nextEl: ".custom-next", prevEl: ".custom-prev" }}
+              pagination={{ clickable: true }}
+              autoplay={{ delay: 3000 }}
+              loop
+            >
+              {/* SwiperSlides */}
+              <SwiperSlide >
                 <article className="flex flex-col justify-center items-center gap-2">
                   <Image
                     src="/carrossel-1_compress.jpg"
@@ -302,16 +304,16 @@ export default function Home() {
                 </article>
               </SwiperSlide>
 
-              </Swiper>
+            </Swiper>
 
-              <button className="hidden md:flex ml-[-60px] custom-prev absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-800 dark:bg-gray-300 text-white dark:text-black p-3 rounded-full shadow-lg z-10">
-                <LuArrowLeft />
-              </button>
-              <button className="hidden md:flex mr-[-60px] custom-next absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-800 dark:bg-gray-300 text-white dark:text-black p-3 rounded-full shadow-lg z-10">
-                <LuArrowRight />
-              </button>
-            </div>
-          </section>
+            <button className="hidden md:flex ml-[-60px] custom-prev absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-800 dark:bg-gray-300 text-white dark:text-black p-3 rounded-full shadow-lg z-10">
+              <LuArrowLeft />
+            </button>
+            <button className="hidden md:flex mr-[-60px] custom-next absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-800 dark:bg-gray-300 text-white dark:text-black p-3 rounded-full shadow-lg z-10">
+              <LuArrowRight />
+            </button>
+          </div>
+        </section>
 
         <section className="w-full flex justify-center items-center py-10 bg-white px-4">
           <iframe
